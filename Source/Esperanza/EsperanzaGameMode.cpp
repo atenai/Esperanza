f36524cc1db14f974a18d21b0a7ac49a4d5cf9e1
LAPTOP-LA51FEJM\alfam <alfamod47@yahoo.co.jp>
2023-07-01 02:15:12 +0900

    1

---
+++ Source/Esperanza/EsperanzaGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EsperanzaGameMode.h"
+#include "EsperanzaCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AEsperanzaGameMode::AEsperanzaGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
