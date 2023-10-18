f1e174a263574c1388d2edb2286dc206566a6e99
LAPTOP-LA51FEJM\alfam <alfamod47@yahoo.co.jp>
2023-10-18 23:47:49 +0900

    デフォルトプロパティ警告とエラーを修正

--- Source/Esperanza/EsperanzaGameMode.cpp
+++ Source/Esperanza/EsperanzaGameMode.cpp
@@ -10,7 +10,7 @@ AEsperanzaGameMode::AEsperanzaGameMode()
 	// set default pawn class to our Blueprinted character
 	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
 	//エスペランサゲームモードでスタートする際に生成するキャラクターをここで選択する（ファイルパス形式）
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_Player"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/Player/BP_Player"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
